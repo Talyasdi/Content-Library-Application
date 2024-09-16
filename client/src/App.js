e3f64af82b20d00918dba2b549eb7df3f6131a57
Carmela <carmelaroz@gmail.com>
2024-09-16 17:11:20 +0300

    dashboard is in progress

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Home from './pages/HomePage/HomePage';
 import styles from './styles/App.module.css';
+import UserContentDashboard from './components/ContentDashboard/ContentDashboard';
 
 function App() {
   return (
@@ -11,11 +12,15 @@ function App() {
           <img src="/project-logo.png" alt="Logo" className={styles.appLogo} />
           <nav className={styles.appNav}>
             <Link to="/" className={styles.appLink}>Home</Link>
+            <Link to="/dashboard">
+          <button className={styles.dashboardButton}>My Dashboard</button>
+          </Link>
           </nav>
         </header>
         <main className={styles.main}>
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/dashboard" element={<UserContentDashboard />} />
           </Routes>
         </main>
         <footer className={styles.footer}>
