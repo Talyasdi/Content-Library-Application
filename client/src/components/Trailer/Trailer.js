c371002ff4f8ced7a5dc86ee80177af306e08952
RuqayaKhalil <ruqayakamal18199@gmail.com>
2024-09-21 17:13:00 +0300

    save before merge main with fixed filter component

--- client/src/components/Trailer/Trailer.js
+++ client/src/components/Trailer/Trailer.js
@@ -1,18 +1,48 @@
 import React from 'react';
 import ReactPlayer from 'react-player'; 
+import styles from '../TrailerLibraryView/TrailerView.module.css'
+
 
 const Trailer = ({ trailer }) => {
+
+  const getEmbeddableLink = (link) => {
+    const youtubeRegex = /(?:youtube\.com\/watch\?v=|youtu\.be\/)([\w-]+)/;
+    const match = link.match(youtubeRegex);
+    if (match && match[1]) {
+      return `https://www.youtube.com/embed/${match[1]}`;
+    }
+    return link; // return the original link if it's not YouTube
+  };
+
+
   return (
     <div style={trailerBoxStyle}>
       <h2>{trailer.trailerName}</h2>
+
+      {trailer.link && (
+        <div className={styles.videoContainer}>
+          <iframe 
+            className={styles.trailerVideo}
+            width="560" 
+            height="315" 
+            src={getEmbeddableLink(trailer.link)} 
+            title="Trailer video" 
+            frameBorder="0" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+            allowFullScreen
+          ></iframe>
+        </div>
+      )}
+      
       <p><strong>Genres:</strong> {trailer.genres.join(', ')}</p>
       <p><strong>Min Age Limit:</strong> {trailer.minAgeLimit}</p>
       <p><strong>Release Year:</strong> {trailer.releaseYear}</p>
       <p><strong>Cast:</strong> {trailer.cast.join(', ')}</p>
-      <a href={trailer.link} target="_blank" rel="noopener noreferrer">Watch Trailer</a>
-      
-      
+      {/* <a href={trailer.link} target="_blank" rel="noopener noreferrer">Watch Trailer</a> */}
+
+
       {/* Render the trailer video using ReactPlayer */}
+      
       {/* {trailer.link && (
        <div className={styles.videoContainer}>
           <ReactPlayer 
--- client/src/context/TrailersContext.js
+++ client/src/context/TrailersContext.js
@@ -1,14 +1,23 @@
 import React, { createContext, useState, useEffect } from 'react';
 import api from '../services/api';
+import {useAuthContext} from '../hooks/useAuthContext'
 
 const TrailersContext = createContext();
 
 const TrailersProvider = ({ children }) => {
   const [trailers, setTrailers] = useState([]);
 
+  const {user} = useAuthContext(); 
+
   const getAllTrailers = async () => {
     try {
-      const response = await api.get('/trailer/trailers');
+      const response = await api.get('/trailer/trailers',{
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+    });
+
+    
       setTrailers(response.data);
     } catch (error) {
       console.error('Error fetching trailers:', error);
