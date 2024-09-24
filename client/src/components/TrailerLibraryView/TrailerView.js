fd5925a395246fe42ccd837e6d3a7144ce0b57f1
RuqayaKhalil <ruqayakamal18199@gmail.com>
2024-09-24 22:02:14 +0300

    add video to trailer card preview

--- client/src/components/TrailerLibraryView/TrailerView.js
+++ client/src/components/TrailerLibraryView/TrailerView.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './TrailerView.module.css';
+import ReactPlayer from 'react-player'; 
 
 // trailer card in library view
 const Trailer = ({ trailer, activePage }) => {
@@ -13,12 +14,24 @@ const Trailer = ({ trailer, activePage }) => {
       <div className={styles.trailerCard} key={trailer._id}>
         <h2 className={styles.trailerName}>{trailer.trailerName}</h2>
 
+      {/* Render the trailer video using ReactPlayer */}
+      {trailer.link && (
+        <div className={styles.videoContainer}>
+          <ReactPlayer 
+            url={trailer.link}  // ReactPlayer will handle different platforms
+            className={styles.trailerVideo}
+            width="100" 
+            height="60"
+            controls       // Show controls for play, pause, etc.
+            playing={false} // Auto-play is off by default
+          />
+        </div>
+      )}
         <ul className={styles.genresList}>
           {trailer.genres.map((genre, index) => (
             <li key={index} className={styles.genreItem}>{genre}</li>
           ))}
         </ul>
-        <p className={styles.trailerUserName}>Uploaded By {trailer.userName}</p>
       </div>
     </Link>
   );
