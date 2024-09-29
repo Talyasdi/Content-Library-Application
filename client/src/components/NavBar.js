fcee368812ff1763c42f6a41236a44a5b16ad315
ofirgor <ofirgormes@gmail.com>
2024-09-29 14:10:20 +0300

    added logo

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import { useLogout } from "../hooks/useLogout";
 import { useAuthContext } from "../hooks/useAuthContext";
 import { useState } from "react";
+import logo from "../assets/logo.png";
 
 const NavBar = () => {
   const { logout } = useLogout();
@@ -17,38 +18,56 @@ const NavBar = () => {
     setMenuOpen(!menuOpen);
   };
 
-  // Function to close the menu after an action
   const closeMenu = () => {
     setMenuOpen(false);
   };
 
   return (
     <header>
-      <div className="container">
-        <nav>
+      <nav>
+        <div className="auth-menu-container">
           {!user && (
-            <div>
-              <Link to="/signup">Signup</Link>
+            <div className="auth-links">
+              <Link className="signup-link" to="/signup">
+                Signup
+              </Link>
               <Link to="/login">Login</Link>
             </div>
           )}
           {user && (
             <div className="user-menu">
-              <span className="menu-icon" onClick={toggleMenu}>&#9776;</span>
-              <span className="user-menu">
-                Welcome {user.userName}! 👋
+              <span className="menu-icon" onClick={toggleMenu}>
+                &#9776;
               </span>
+              <span className="user-menu" onClick={toggleMenu}>Welcome {user.userName}! 👋</span>
               {menuOpen && (
                 <div className="dropdown-menu">
-                  <Link to="/" onClick={closeMenu}>HomePage</Link>
-                  <Link to="/dashboard" onClick={closeMenu}>My Dashboard</Link>
-                  <button className="logout-btn" onClick={() => { handleLogout(); closeMenu(); }}><i class="fas fa-sign-out-alt"></i>Logout</button>
+                  <Link to="/" onClick={closeMenu}>
+                    HomePage
+                  </Link>
+                  <Link to="/dashboard" onClick={closeMenu}>
+                    My Dashboard
+                  </Link>
+                  <button
+                    className="logout-btn"
+                    onClick={() => {
+                      handleLogout();
+                      closeMenu();
+                    }}
+                  >
+                    <i class="fas fa-sign-out-alt"></i>Logout
+                  </button>
                 </div>
               )}
             </div>
           )}
-        </nav>
-      </div>
+        </div>
+        <div className="logo">
+          <Link to={user ? "/" : "/login"} onClick={closeMenu}>
+            <img src={logo} alt="Trailer Vault Logo" />
+          </Link>
+        </div>
+      </nav>
     </header>
   );
 };
