526d37199848273f4089d80f7c3faae3ac018164
Talyasdi <92432346+Talyasdi@users.noreply.github.com>
2024-12-06 15:13:55 +0200

    Sorting and filtering implement (#2)
    * merging all features
    
    * merging all features
    
    * Changed some styles
    
    * Fixing last changes
    
    * Fixing last changes (#29)
    
    * Fixing last changes

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -2,15 +2,13 @@ import React from 'react';
 import FilterSortBox from '../components/FilterAndSort/FilterAndSortBox';
 import LibraryView from './LibraryViewPage/LibraryViewPage';
 
-const Home = ({ successMessage }) => { // Receive success message as prop
+const Home = () => { // Receive success message as prop
   return (
     <div className="home">
       <FilterSortBox />
-      {successMessage && <div className="success">{successMessage}</div>} {/* Display success message */}
     </div>    
   );
 };
 
+
 export default Home;
-
-
